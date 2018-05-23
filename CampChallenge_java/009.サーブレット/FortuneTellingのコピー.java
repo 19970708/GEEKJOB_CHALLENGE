6adbfd7c2690ed072fa6883435219cb353c030b7
波多野陽平 <>
2018-05-23 09:32:40 +0900

    サーブレットまで

--- CampChallenge_java/009.サーブレット/FortuneTellingのコピー.java
+++ CampChallenge_java/009.サーブレット/FortuneTellingのコピー.java
@@ -32,15 +32,6 @@ public class FortuneTelling extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet FortuneTelling</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet FortuneTelling at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
             String luckList[] = {"大吉","中吉","小吉","吉","半吉","末小吉","凶","小凶","半凶","末凶","大凶"};
             Random rand=new Random();
             Integer index=rand.nextInt(luckList.length);
